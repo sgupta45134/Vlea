910fac82f0ea77bd87c77eedf9eec3ab10d9bcd6
Sudhanshu Gupta <sudhanshug5@gmail.com>
2020-07-01 23:25:49 +0530

    adding table and refund process

--- local/stripepayment/manualcredit.php
+++ local/stripepayment/manualcredit.php
@@ -25,19 +25,54 @@
  */
 require_once('../../config.php');
 require_login();
-$userid = optional_param('id',0, PARAM_INT);
+$userid = optional_param('id', 0, PARAM_INT);
+
+?>
+        <script>
+function onSelectcourse(select) {
+  var plan = select;
+  $('#id_selected_plan').val(plan);
+};
+</script>
+<?php
 require_once($CFG->dirroot . '/user/editlib.php');
 require_once('manualcredit_form.php');
 
 $PAGE->set_url("/local/stripepayment/manualcredit.php?id=$userid");
 $PAGE->set_context(context_system::instance());
 
-$mform_manualcredit = new manualcredit_form(null,$userid);
+$mform_manualcredit = new manualcredit_form(null, $userid);
 
 if ($mform_manualcredit->is_cancelled()) {
-    redirect("$CFG->wwwroot/admin/user.php");
-
-} else if ($creditdata = $mform_manualcredit->get_data()) {
+  redirect("$CFG->wwwroot/admin/user.php");
+}
+else if ($creditdata = $mform_manualcredit->get_data()) {
+  if ($creditdata->submitbutton == 'Refund Credits') {
+          $userid = $creditdata->userid;
+      if(empty($creditdata->selected_plan)){
+//        print_error('please_select_plan','local_stripepayment', "$CFG->wwwroot/local/stripepayment/manualcredit.php?id=$userid");
+        redirect("$CFG->wwwroot/local/stripepayment/manualcredit.php?id=$userid", get_string('please_select_plan', 'local_stripepayment'));
+      }
+    //update the credit in the user_credit table
+      $records = $DB->get_record('user_credits',array('id'=>$creditdata->selected_plan));
+      $left_credit = $records->total_credit_left;
+      if(($creditdata->credits_refund + $left_credit) > $records->total_credit){
+         redirect("$CFG->wwwroot/local/stripepayment/manualcredit.php?id=$userid", get_string('refund_exceed', 'local_stripepayment'));
+      }
+      $new_left_credit =  $left_credit + $creditdata->credits_refund;
+      $sql = "update {user_credits} set total_credit_left = $new_left_credit where id = $creditdata->selected_plan";
+      $DB->execute($sql);
+     //update info data
+      $fieldid = $DB->get_field('user_info_field', 'id', array('shortname' => 'user_credits'));
+      $record = $DB->get_record('user_info_data', array('fieldid' => $fieldid, 'userid' => $userid));
+      $balance = $record->data;
+      $balance += $creditdata->credits_refund;
+      $sql = "update {user_info_data} set data = $balance where id=$record->id";
+      $DB->execute($sql);
+      redirect("$CFG->wwwroot/local/stripepayment/manualcredit.php?id=$userid", get_string('credit_refund', 'local_stripepayment'));
+    
+  }
+  elseif ($creditdata->submitbutton == 'Add Credits') {
     $data = new stdClass();
     $userid = $creditdata->userid;
     $data->userid = $userid;
@@ -49,40 +84,79 @@ if ($mform_manualcredit->is_cancelled()) {
     $data->timemodified = time();
     $data->payment_type = 'manual';
     $DB->insert_record('user_credits', $data);
-    
+
     $new_credit = $creditdata->manual_credit;
     $fieldid = $DB->get_field('user_info_field', 'id', array('shortname' => 'user_credits'));
-    if ($record =  $DB->get_record('user_info_data', array('fieldid' => $fieldid, 'userid' => $userid))) {
-    $balance = $record->data;
-    $balance += $new_credit;
-    $sql = "update {user_info_data} set data = $balance where id=$record->id";
-    $DB->execute($sql);
+    if ($record = $DB->get_record('user_info_data', array('fieldid' => $fieldid, 'userid' => $userid))) {
+      $balance = $record->data;
+      $balance += $new_credit;
+      $sql = "update {user_info_data} set data = $balance where id=$record->id";
+      $DB->execute($sql);
+    }
+    else {
+      $payment = new stdClass();
+      $payment->userid = $userid;
+      $payment->fieldid = $fieldid;
+      $payment->data = $new_credit;
+      $payment->dataformat = 0;
+      $insertid = $DB->insert_record('user_info_data', $payment);
+    }
   }
   else {
-    $payment = new stdClass();
-    $payment->userid = $userid;
-    $payment->fieldid = $fieldid;
-    $payment->data = $new_credit;
-    $payment->dataformat = 0;
-    $insertid = $DB->insert_record('user_info_data', $payment);
+    redirect("$CFG->wwwroot/local/stripepayment/manualcredit.php?id=$userid", get_string('some_issue', 'local_stripepayment'));
   }
-    redirect("$CFG->wwwroot/local/stripepayment/manualcredit.php?id=$userid", get_string('credit_assigned', 'local_stripepayment'));
-} else {
-    
+  redirect("$CFG->wwwroot/local/stripepayment/manualcredit.php?id=$userid", get_string('credit_assigned', 'local_stripepayment'));
+}
+else {
 
-//$coursename = $DB->get_field('course', 'shortname', array('id' => $courseid));
-//$courselink = new moodle_url('/course/view.php', array('id'=>$courseid));
-//$PAGE->navbar->add($coursename,$courselink);
-//$PAGE->navbar->add($feeddata);
-//
-$title = get_string('manual_credit', 'local_stripepayment');
-$PAGE->set_title($title);
-$PAGE->set_heading($SITE->fullname);
-$user = $DB->get_record('user',array('id' => $userid));
-$userfullname = fullname($user, true);
+  $title = get_string('manual_credit', 'local_stripepayment');
+  $PAGE->set_title($title);
+  $PAGE->set_heading($SITE->fullname);
+  $user = $DB->get_record('user', array('id' => $userid));
+  $userfullname = fullname($user, true);
 
-echo $OUTPUT->header();
-echo $OUTPUT->heading($userfullname);
-$mform_manualcredit->display();
-echo $OUTPUT->footer();
+  echo $OUTPUT->header();
+  echo $OUTPUT->heading($userfullname);
+  $fieldid = $DB->get_field('user_info_field', 'id', array('shortname' => 'user_credits'));
+  $balance = $DB->get_field('user_info_data', 'data', array('fieldid' => $fieldid, 'userid' => $userid));
+  $credits = ($balance > 0) ? $balance : 0;
+  $total_credits = get_string('total_credits', 'local_stripepayment') . $credits;
+  echo $OUTPUT->heading($total_credits);
+  echo "</br></br>";
+  echo $OUTPUT->heading(get_string('purchase_history', 'local_stripepayment'));
+  echo html_writer::table(active_plans_user($userid));
+  echo "</br></br>";
+  $mform_manualcredit->display();
+
+  echo $OUTPUT->footer();
+}
+
+function active_plans_user($userid) {
+  global $DB;
+  $table = new html_table();
+  $table->head = array(get_string('payment_method', 'local_stripepayment'), get_string('purchased_credit', 'local_stripepayment'),
+    get_string('credit_left', 'local_stripepayment'),
+    get_string('purchase_date', 'local_stripepayment'), get_string('expiry_date', 'local_stripepayment'));
+  $table->size = array('20%', '20%', '20%', '20%', '20%');
+  $table->attributes = array('class' => 'display');
+  $table->align = array('center', 'center', 'center', 'center', 'center');
+  $table->width = '100%';
+  $records = $DB->get_records('user_credits', array('status' => 1, 'userid' => $userid, 'expire' => 0));
+  if (!empty($records)) {
+    foreach ($records as $recordkey => $recordvalue) {
+      $row = array();
+      $payment_type = array('credit_card' => 'Stripe Top Up', 'manual' => 'Manual');
+      $row[] = $payment_type[$recordvalue->payment_type];
+      $row[] = $recordvalue->total_credit;
+      $row[] = $recordvalue->total_credit_left;
+      $row[] = date("d-M-Y", $recordvalue->timemodified);
+      $row[] = date("d-M-Y", strtotime('+30 days', $recordvalue->timemodified));
+
+      $table->data[] = $row;
+    }
+  }
+  else {
+    $table->data[] = array('', '', get_string('notfound', 'block_systemreports'), '', '');
+  }
+  return $table;
 }
--- local/stripepayment/manualcredit_form.php
+++ local/stripepayment/manualcredit_form.php
@@ -30,10 +30,54 @@ require_once($CFG->libdir.'/formslib.php');
 
 class manualcredit_form extends moodleform {
     function definition() {
-        global $USER, $CFG, $PAGE;
-         
+        global $USER, $CFG, $PAGE, $DB;
+        
+         $PAGE->requires->js('/local/stripepayment/module.js');
         $mform = $this->_form;
         $userid = $this->_customdata;
+              $mform->addElement('header', 'refund', get_string('refund_credit', 'local_stripepayment'), ''); 
+        $records = $DB->get_records('user_credits',array('status' => 1,'userid' => $userid,'expire' =>0));
+        $active_plans = array();
+        
+        foreach($records as $recordkey => $recordvalue){
+          if($recordvalue->total_credit_left == $recordvalue->total_credit) 
+            continue;
+          $key = "ORD-".$recordvalue->id; 
+          $active_plans[$recordkey] = $key;  
+        }
+        
+        $options = "<option value=''>" . get_string('select_plan', 'local_stripepayment') .
+                   "</option>";
+        foreach ($active_plans as $i => $value) {
+            $options .= "<option value='{$i}'>$value</option>";
+        }
+
+        $mform->addElement('html', '<div class = "row mr_top_items">');
+
+        $mform->addElement('html', '<div class = "col-sm-5">');
+
+        $select = "<select class='coursevars form-control' onchange='onSelectcourse(this.value)'>
+                 $options</select>";
+        $htmlcourse = "<div class='fitem row'><div class='fitemtitle col-sm-6'>Consumed Plans:</div><div class='felement col-sm-6'>
+                 $select</div></div>";
+        $mform->addElement('html', $htmlcourse);
+
+        $mform->addElement('html', '</div>');
+        
+       $mform->addElement('text', 'credits_refund', get_string('credits_refund', 'local_stripepayment'), 'maxlength="100" size="25"');
+       $mform->setType('credits_refund', PARAM_RAW);
+       $mform->addRule('credits_refund', 'Numeric Values Only', 'numeric', null, 'client');
+       $mform->addElement('html', '<div class = "my_hidden_class">');
+       $mform->addElement('text','selected_plan',get_string('selected_plans','local_stripepayment'),$attributes='size=20,maxlength=250');
+       $mform->setType('selected_plan', PARAM_RAW);
+       $mform->addElement('html', '</div>');
+       $mform->addElement('hidden', 'userid', $userid);
+       $mform->setType('userid', PARAM_INT);
+        // buttons
+        $this->add_action_buttons(true, get_string('submitrefunddata', 'local_stripepayment'));
+        
+        
+        
         $mform->addElement('header', 'credit', get_string('manual_credit_expire', 'local_stripepayment'), '');
         $radioarray=array();
         $radioarray[] =& $mform->createElement('radio', 'manual_credit', '', get_string('plan1', 'local_stripepayment'), '180');
