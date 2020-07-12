35fd0d669f305d4fbd4955b9ff74f64be194c2e3
Sudhanshu Gupta <sudhanshug5@gmail.com>
2020-07-12 17:32:40 +0530

    inital commit for send expire mail reminder

---
+++ local/expire_reminder_email/lang/en/local_expire_reminder_email.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_email', language 'en'
+ */
+
+$string['crontask'] = 'Expire Reminder Email';
+$string['pluginname'] = 'Expire Reminder Email';
+$string['expire_reminder_email_subject'] = '[VLE Academy] Your credits are going to expire on {$a->expiredate}';
+$string['expire_reminder_email_message'] = 'Dear {$a->fullanme},
+  
+On behalf of VLEAcademy, we would like to express our heartfelt thanks for your support.
+
+We are writing to gently remind you that your child’s account credits with us expires in 7 days, and we sincerely hope that you will continue to join us for more great tuition programs for your child.
+Good news! There’s still time to renew, and it is as easy as ever with these options:
+
+1.	Make a PayNow transfer for the Basic $180 or Advance $320 package for another 30 days to our UEN 202012386K and send us a screenshot to Mobile 9172 3109. 
+
+We will immediately upload the credits into your account or
+
+2.	Visit our website at www.vleacademy.com to repurchase the package and pay with your Credit/Debit card. 
+
+More good news! Get rewarded with a small token from us when you refer your friends/relatives to join us on this fun and interactive learning journey. 
+
+VLEAcademy values you and helps you meet your child’s potential goals in attaining their best professional academic achievement.
+
+Please let us know if you have questions or concerns.
+
+We look forward once again to your support.
+
+Thank you.
+Sincerely,
+
+VLEAcademy Pte Ltd
+Hotline : 9172 3109
+Website : http://www.vleacademy.com';
